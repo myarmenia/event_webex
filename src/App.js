02ce45006a162f76fc35ca90870d3e1466e986ac
unknown <mio.dav@bk.ru>
2024-06-03 11:41:57 +0300

    star

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import './App.css';
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import HomeWraper from './page/HomeWraper';
-import { Tikets } from './components';
-import Wedding1 from './components/Wedding1/Wedding1';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import HomeWraper from "./page/HomeWraper";
+import { Tikets } from "./components";
+import Wedding1 from "./components/Wedding1/Wedding1";
+import { useEffect, useState } from "react";
 
 function App() {
-   const [musicModal, setMusicModal] = useState(false);
+  const [musicModal, setMusicModal] = useState(false);
 
-   const leng = localStorage.getItem('lang') != null ? localStorage.getItem('lang') : 'am';
+  const leng =
+    localStorage.getItem("lang") != null ? localStorage.getItem("lang") : "am";
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-   useEffect(() => {
-      pathname == '/' && navigate(`/${leng}/`);
-   }, []);
+  useEffect(() => {
+    pathname == "/" && navigate(`/${leng}/`);
+  }, []);
 
-   return (
-      <div className="App">
-         <Routes>
-            <Route path="/" element={<HomeWraper />}>
-               <Route path=":lang">
-                  <Route path="wedding1">
-                     <Route index element={<Wedding1 />} />
-                     <Route path=":name" element={<h1>aaaaaaaaa</h1>} />
-                  </Route>
-                  <Route
-                     path="tikets"
-                     element={
-                        <div style={{ backgroundColor: 'black' }}>
-                           <Tikets />
-                        </div>
-                     }
-                  />
-               </Route>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomeWraper />}>
+          <Route path=":lang">
+            <Route path="wedding1">
+              <Route index element={<Wedding1 />} />
+              <Route path=":name" element={<h1>aaaaaaaaa</h1>} />
             </Route>
-         </Routes>
-      </div>
-   );
+            <Route
+              path="tikets"
+              element={
+                <div style={{ backgroundColor: "black" }}>
+                  <Tikets />
+                </div>
+              }
+            />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
